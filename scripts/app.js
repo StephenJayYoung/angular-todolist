29caae3fb0d5956c7d60368e2c56c90cdbd72b35
Stephen Young <stephenjayyoung@gmail.com>
2016-03-24 10:07:32 -0700

    added in some fake data, directives

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,16 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
   console.log("hello there! This is the hello world controller function in the mainCtrl");
   };
+  
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "water the dog"},
+    {"name": "feed the lawn"},
+    {"name": "pay the billz"},
+    {"name": "run"},
+    {"name": "swim"} 
+  ]
+  
+  
+  
 });
